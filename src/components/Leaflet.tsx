d358e2a1b69b7f5a8385ff8254b8c31e6f62d500
Jingu-web <multiNomaFeBe.rjrrr@gmail.com>
2021-10-14 08:00:18 +0900

    🩹 ズームの倍率とマップのセンターを日本に修正

--- src/components/Leaflet.tsx
+++ src/components/Leaflet.tsx
@@ -1,13 +1,13 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { Map, TileLayer } from "react-leaflet";
 
 export interface LeafletProps {}
 
-const position: [number, number] = [0, 0];
+const position: [number, number] = [35.66572, 139.731];
 
 const Leaflet: FC<LeafletProps> = () => {
   return (
-    <Map center={position} zoom={13} style={{ height: "100vh" }}>
+    <Map center={position} zoom={3} style={{ height: "100vh" }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
