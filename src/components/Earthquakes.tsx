20a9ed3950b8766f0f9c1b4cdf6bed8036de6063
Jingu-web <multiNomaFeBe.rjrrr@gmail.com>
2021-10-15 18:41:49 +0900

    ✨ 地震の大きさによって色と円の半径が変えた

--- src/components/Earthquakes.tsx
+++ src/components/Earthquakes.tsx
@@ -4,6 +4,7 @@ import L, { CircleMarker, LatLng } from "leaflet";
 
 import { getEarthquakes } from "../services/getEarthquakes";
 import { IFeature } from "../models/IFEature";
+import { geojsonMarkerOptions } from "../utils/geojsonMarkerOptions";
 
 export interface EarthquakesProps {
   leaflet: {
@@ -15,21 +16,12 @@ const Earthquakes: FC<EarthquakesProps> = ({ leaflet }) => {
   const [earthquakes, setEarthquakes]: any = useState([]);
   console.log(earthquakes);
 
-  const geojsonMarkerOptions = {
-    radius: 8,
-    fillColor: "#ff7800",
-    color: "#000",
-    weight: 1,
-    opacity: 1,
-    fillOpacity: 0.8,
-  };
-
   const onEachFeature = (feature: IFeature, layer: CircleMarker) => {
     let popupContent = `
             <h3>${feature.properties.title}</h3>
             <b>Place</b>: ${feature.properties.place} <br>
             <b>Magnitude</b>: ${feature.properties.mag} Richter<br>
-            <b>Details</b>: <a href=${feature.properties.url}>more details here</a>
+            <b>Details</b>: <a href=${feature.properties.url} target="_blank">click here to find more details</a>
         `;
 
     if (feature.properties) layer.bindPopup(popupContent);
@@ -38,7 +30,8 @@ const Earthquakes: FC<EarthquakesProps> = ({ leaflet }) => {
   L.geoJSON(earthquakes.features, {
     onEachFeature,
     pointToLayer: function (feature: IFeature, latlng: LatLng) {
-      return L.circleMarker(latlng, geojsonMarkerOptions);
+      const magnitude = feature.properties.mag;
+      return L.circleMarker(latlng, geojsonMarkerOptions(magnitude));
     },
   }).addTo(leaflet.map);
 
