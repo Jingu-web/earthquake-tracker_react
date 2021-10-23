44c6d506d52d601ea8c4e79c487bff6ad458baa0
Jingu-web <multiNomaFeBe.rjrrr@gmail.com>
2021-10-23 09:30:27 +0900

    💬 ポップアップに日本時間で地震発生時を刻表示

--- src/components/Earthquakes.tsx
+++ src/components/Earthquakes.tsx
@@ -5,6 +5,7 @@ import L, { LatLng, Layer } from "leaflet";
 import { getEarthquakes } from "../services/getEarthquakes";
 import { IFeature } from "../models/IFEature";
 import { geojsonMarkerOptions } from "../utils/geojsonMarkerOptions";
+import { timeConverter } from "../utils/timeConverter";
 
 export interface EarthquakesProps {
   leaflet: {
@@ -19,12 +20,18 @@ const Earthquakes: FC<EarthquakesProps> = ({ leaflet }) => {
   const onEachFeature = (feature: IFeature, layer: Layer) => {
     let popupContent = `
             <h3>${feature.properties.title}</h3>
+            <b>発生時刻(JST)</b>: ${timeConverter(
+              feature.properties.time,
+              9
+            )} <br>
             <b>震源地</b>: ${feature.properties.place} <br>
-            <b>緯度</b>: ${feature.geometry.coordinates[1]}
+            <b>緯度</b>: ${feature.geometry.coordinates[1]} <br>
             <b>経度</b>: ${feature.geometry.coordinates[0]} <br>
             <b>深さ</b>: ${feature.geometry.coordinates[2]} km <br>
             <b>マグニチュード</b>: ${feature.properties.mag} Richter <br>
-            <b>詳細</b>: <a href=${feature.properties.url} target="_blank">詳細はココをクリック</a>
+            <b>詳細</b>: <a href=${
+              feature.properties.url
+            } target="_blank">詳細はココをクリック</a>
         `;
 
     if (feature.properties) layer.bindPopup(popupContent);
