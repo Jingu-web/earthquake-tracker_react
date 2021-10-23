2051e23b134e31e174aba268443ba1fa5c3bb42d
Jingu-web <multiNomaFeBe.rjrrr@gmail.com>
2021-10-23 09:14:50 +0900

    ✨ 協定世界時 (UTC)に変える関数を定義

---
+++ src/utils/timeConverter.ts
@@ -0,0 +1,6 @@
+export const timeConverter = (time: number, offset: number): string => {
+  const d = new Date(time);
+  const utc = d.getTime() + d.getTimezoneOffset() * 60000;
+  const nd = new Date(utc + 3600000 * offset);
+  return nd.toLocaleString();
+};
