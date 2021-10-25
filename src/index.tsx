c58c87df5edf0e46a735c89376bb79c6fe77dc10
Jingu-web <multiNomaFeBe.rjrrr@gmail.com>
2021-10-26 08:29:41 +0900

    ✨ reducerファイルの作成

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
-import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import "./index.css";
+import "leaflet/dist/leaflet.css";
 import App from "./App";
 
-import "leaflet/dist/leaflet.css";
+import store from "./store";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
