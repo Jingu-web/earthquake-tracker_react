9c7f12f3f9306abb327c2c218005243bcc4c9870
Jingu-web <multiNomaFeBe.rjrrr@gmail.com>
2021-10-26 12:09:49 +0900

    🤡 ヘッダーのモック

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,55 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import { connect } from "react-redux";
 
-export interface NavbarProps {}
+import { changeQuery } from "../store/actions";
 
-const Navbar: FC<NavbarProps> = () => {
+export interface NavbarProps {
+  changeQuery: (arg1: any) => void;
+}
+
+const Navbar: FC<NavbarProps> = ({ changeQuery }) => {
+  const [query, setQuery] = useState("");
+
   return (
-    <div className="flex justify-between bg-green-200 pt-8">
-      Earthquake
-      <div>
-        <button type="submit">Search</button>
+    <nav className="flex justify-between bg-green-200 pt-8">
+      <div className="navbar-brand mr-auto mr-lg-0">Earthquakes</div>
+      <button
+        className="navbar-toggler p-0 border-0"
+        type="button"
+        data-toggle="offcanvas"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="navbar-collapse offcanvas-collapse">
+        <ul className="navbar-nav mr-auto mt-1"></ul>
+        <form
+          className="form-inline my-2 my-lg-0"
+          onSubmit={(e) => {
+            changeQuery(query);
+            e.preventDefault();
+          }}
+        >
+          <input
+            className="form-control mr-sm-2"
+            type="text"
+            placeholder="Search..."
+            aria-label="Search"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button
+            className="btn btn-outline-success my-2 my-sm-0"
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
       </div>
-    </div>
+    </nav>
   );
 };
 
-export default Navbar;
+const mapDispatchToProps = { changeQuery };
+
+export default connect(null, mapDispatchToProps)(Navbar);
